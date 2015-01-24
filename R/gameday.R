a2f86d6920e2eac95dff069a034380ef6f3a2503
Ed Copony <ecopony@yahoo.com>
2015-01-24 10:49:18 -0800

    Basic spray chart for player and year.

--- R/gameday.R
+++ R/gameday.R
@@ -18,3 +18,12 @@ loadHitsForYear <- function(year) {
   res <- dbSendQuery(con, query)
   fetch(res, n=-1)
 }
+
+plotHitsForPlayerAndYear <- function(player, year) {
+  hits <- loadHitsForYear(year)
+  player.hits <- subset(hits, batter == player)
+  plot(0:250, -250:0, type="n", bg="white")
+  lines(c(125, 150, 125, 100, 125), c(-210, -180, -150, -180, -210), col=c("black"))
+  points(subset(player.hits$x, player.hits$type=="H"), subset(-player.hits$y, player.hits$type=="H"), pch=10, col=c("red"))
+  points(subset(player.hits$x, player.hits$type=="O"), subset(-player.hits$y, player.hits$type=="O"), pch=10, col=c("blue"))
+}
