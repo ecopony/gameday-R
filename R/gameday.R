4acdf98e2a5ac9f7c7f0de783a1f93e9fa2b7652
Ed Copony <ecopony@yahoo.com>
2015-01-24 10:10:38 -0800

    Load pitches for year.

---
+++ R/gameday.R
@@ -0,0 +1,12 @@
+# Just getting going here.
+#
+# Starting with some basics to load data from a PostgreSQL database.
+# See https://github.com/ecopony/pggameday getting it filled with data.
+
+loadPitchesForYear <- function(year) {
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, dbname="go-gameday", host="localhost")
+  query <- paste("select * from pitches where year =",year, sep="")
+  res <- dbSendQuery(con, query)
+  fetch(res, n=-1)
+}
